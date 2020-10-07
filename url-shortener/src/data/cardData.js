841fbd283292c05bd0cd4328d9bca90caa8025b9
Rachel <rjcrter11@gmail.com>
2020-10-07 10:16:15 -0500

    moves card data to separate file, makes card component

---
+++ url-shortener/src/data/cardData.js
@@ -0,0 +1,24 @@
+import iconBrand from '../assets/icon-brand-recognition.svg'
+import iconDetailed from '../assets/icon-detailed-records.svg'
+import iconCustom from '../assets/icon-fully-customizable.svg'
+
+export const cards = [
+    {
+        id: 1,
+        title: "Brand Recognition",
+        text: "Boost your brand recognition with each click. Generic links don't mean a thing. Branded links help instill confidence in your content",
+        icon: iconBrand
+    },
+    {
+        id: 2,
+        title: 'Detailed Records',
+        text: "Gain insights into who is clicking your links.Knowing when and where people engage with your content helps inform better decisions.",
+        icon: iconDetailed
+    },
+    {
+        id: 3,
+        title: 'Fully Customizable',
+        text: "Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.",
+        icon: iconCustom
+    }
+]
